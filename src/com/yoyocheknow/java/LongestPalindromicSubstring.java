d45cfd4778f9bb9b3c416c2d97ad2081419f083a
yoyocheknow <911dangerous@gmail.com>
2017-12-15 12:41:05 +0800

    第五题的第一种解法

--- src/com/yoyocheknow/java/LongestPalindromicSubstring.java
+++ src/com/yoyocheknow/java/LongestPalindromicSubstring.java
@@ -1,5 +1,8 @@
 package com.yoyocheknow.java;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  * 类说明
  * 判断字符串中的最长回文
@@ -17,11 +20,37 @@ package com.yoyocheknow.java;
  * @date 2017/12/14 12:07
  */
 public class LongestPalindromicSubstring {
+    //第一种解法，用递归的方法判断选取的字符串是否是回文，然后循环遍历，最后找找到最长的那个。
+    //这种暴力的解法结果就是。。。超时。
     public static String longestPalindrome(String s) {
-        int low =0;
+       // int low =0;
         int length=s.length();
+        ArrayList<String> array= new ArrayList<>();
+        for(int low =0;low<length;low++)
+        {
+            for(int high=low;high<length;high++)
+            {
+                if(isPalindromic(low,high,s,high-low+1))
+                {
+                    array.add(s.substring(low,high+1));
+                    //System.out.print(array.toString());
+                }
+            }
+        }
+        String temp="";
+        temp=array.get(0);
+        for(int i=1;i<array.size();i++)
+        {
 
-    return "ss";
+            if(temp.length()<array.get(i).length())
+            {
+                temp=array.get(i);
+                System.out.println("temp="+temp);
+            }
+        }
+
+        System.out.println(array.toString());
+    return temp;
     }
 
     public static boolean isPalindromic(int low,int high,String s,int length)
@@ -38,6 +67,7 @@ public class LongestPalindromicSubstring {
     }
     public static void main(String args[])
     {
-
+        String s =longestPalindrome("abaabb");
+        System.out.println("s="+s);
     }
 }
